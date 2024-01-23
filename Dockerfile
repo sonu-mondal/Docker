41d98597fde5f33b3aa5866ff0aa263dcb2a68ea
Sonu Mondal <sonu.mondal4598@gmail.com>
2024-01-23 23:41:12 +0530

    made changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
+#this is base image
 FROM openjdk:11
+#url for the working directory
 WORKDIR /user/src/myapp
+#copying to working directory
 COPY . /user/src/myapp/
+#compiled the java program
 RUN javac test.java
-CMD ["java", "test"]
+
+CMD ["java","test"]
--- test.java
+++ test.java
@@ -1,10 +1,13 @@
-public class test{
-    public static void printing_system_properties{
+import java.util.Properties;
+
+public class test {
+    public static void printing_system_properties() {
         System.out.println("Printing system properties");
-        properties  prop=System.getProperties();
+        Properties prop = System.getProperties();
         System.out.println(prop);
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         System.out.println("Java program started");
         printing_system_properties();
     }
